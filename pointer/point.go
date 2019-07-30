d3710c5f01a026deeee2573382e8eb89c3dd5529
kesetbasah <robaimuh@gmail.com>
2019-07-30 22:29:02 +0700

    pointer

---
+++ pointer/point.go
@@ -0,0 +1,12 @@
+package main
+
+import(
+	"fmt"
+)
+
+func main(){
+	var1 := 19
+	fmt.Printf("variabel : %v\n", var1)
+	fmt.Printf("variabel : %v\n", &var1)
+	fmt.Printf("variabel type : %T\n", &var1)
+}
